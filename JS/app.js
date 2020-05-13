4c6c3317910e4f07fb9f064d7d9bb28d571fcd96
Shubham <shubhi.g2001@gmail.com>
2020-05-13 15:27:26 +0530

    complete connection to firebase

--- JS/app.js
+++ JS/app.js
@@ -13,15 +13,27 @@
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
     firebase.analytics();
-
-    //script to format the form
-    var droneCode = document.getElementById("drone-code");
-
-    var onCodeSubmit = function (e) {
+    let firestore = firebase.firestore();
 
 
+    let droneIDInput = document.getElementById("drone-id");
+    let hostelInput = document.getElementById("hostel-to");
+    const submitButton = document.getElementById("Submit");
+    const docRef = firestore.doc("data/drone data") ;
 
+    var onSubmit = function () {
+        let droneID = droneIDInput.value;
+        let hostelToGo = hostelInput.value;
+
+        docRef.set({
+            Drone: droneID,
+            deliveryloc: hostelToGo
+        }).then(function () {
+                console.log("yeah")
+        }).catch(function (error) {
+                console.log("Error"  + error)
+        });
     };
 
         //to respond to click
-        droneCode.addEventListener( "" , onCodeSubmit);
+    submitButton.addEventListener( "click" , onSubmit);
