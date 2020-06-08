50275e9c8d0be8e92fd7a9434918d22e9ad2aca0
Shubham <shubhi.g2001@gmail.com>
2020-06-08 16:45:11 +0530

    modified js

--- JS/app.js
+++ JS/app.js
@@ -51,8 +51,42 @@
             trackingID: droneID
         });
 
-        //location.href = "HTML/testloc.html";
+        
+        //JS for communicating with ROS
+
+        var ros = new ROSLIB.Ros({
+            url : 'ws://localhost:9090'
+        });
+        ros.on('connection', function() {
+            console.log('Connected to websocket server.');
+        });
+
+        ros.on('error', function(error) {
+            console.log('Error connecting to websocket server: ', error);
+        });
+
+        ros.on('close', function() {
+            console.log('Connection to websocket server closed.');
+        });
+        //Publishing
+        var lockey = new ROSLIB.Topic({
+            ros: ros,
+            name: '/location_input',
+            messageType: 'std_msgs/String'
+        });
+        var thespot = document.getElementById("hostel-to").value;
+        var hostel = new ROSLIB.Message({
+            data: thespot,
+        });
+        lockey.publish(hostel);
+        console.log('Destination: ', thespot);
+        
+        //reload page
+        location.href = "index.html";
+
     };
+
+
 
     //for data entry on response to tracking form
     var onSubmitTrack = function (e) {
