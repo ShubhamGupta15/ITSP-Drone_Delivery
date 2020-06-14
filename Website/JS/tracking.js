602fb9a6ddc02c2ba6c65059c0438ca0a1bba6a2
Shyam Iyer <56477122+Shaxos453@users.noreply.github.com>
2020-06-14 12:27:29 +0530

    tracking (working, single drone)

--- Website/JS/tracking.js
+++ Website/JS/tracking.js
@@ -1,47 +1,97 @@
-//fetching droneID to track from URL param
-var queryString = decodeURIComponent(window.location.search);
-queryString = queryString.substring(1);
-var trackID = queryString.split("&");
+var ros = new ROSLIB.Ros({
+        url : 'ws://localhost:9090'
+    });
+    ros.on('connection', function() {
+        console.log('Connected to websocket server.');
+    });
 
-function showPosition() {
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showMap, showError);
-    } else {
-        alert("Sorry, your browser does not support HTML5 geolocation.");
-    }
+    ros.on('error', function(error) {
+        console.log('Error connecting to websocket server: ', error);
+    });
+
+    ros.on('close', function() {
+        console.log('Connection to websocket server closed.');
+    });
+
+    //fetching droneID to track from URL param
+    var queryString = decodeURIComponent(window.location.search);
+    queryString = queryString.substring(1);
+    var trackID = queryString.split("&");
+
+    var lat, long;
+
+    //subscription
+    var getloc = new ROSLIB.Topic({
+      ros : ros,
+      name : '/mavros/global_position/global',
+      messageType : 'sensor_msgs/NavSatFix'
+    });
+
+    getloc.subscribe(function(message) {
+      lat = message.latitude;
+      long = message.longitude;
+
+    });
+
+var map, watchId, userPin;
+
+function GetMap()
+{
+    map = new Microsoft.Maps.Map('#myMap', {
+        credentials:'Auy26xShWKNnx2Ko3hic0uOAMBpK0jy_TtfM06-nuGi3WBeUREIlgzJMLqylaDs8'
+    });
+}
+/*
+function StartTracking() {
+    //Add a pushpin to show the user's location.
+    userPin = new Microsoft.Maps.Pushpin(map.getCenter(), { visible: false });
+    map.entities.push(userPin);
+    watchId = navigator.geolocation.watchPosition(UsersLocationUpdated);
+    console.log('started Tracking');
+    console.log('Push '+lat + ' ' +long);
 }
 
-// Define callback function for successful attempt
-function showMap(position) {
-    // Get location data
-    lat = position.coords.latitude;
-    long = position.coords.longitude;
-    var latlong = new google.maps.LatLng(lat, long);
+function UsersLocationUpdated() {
+    var loc = new Microsoft.Maps.Location(lat, long);
 
-    var myOptions = {
-        center: latlong,
-        zoom: 16,
-        mapTypeControl: true,
-        navigationControlOptions: {
-            style:google.maps.NavigationControlStyle.SMALL
-        }
-    }
+    //Update the user pushpin.
+    userPin.setLocation(loc);
+    userPin.setOptions({ visible: true });
+    console.log('Updated '+lat + ' ' +long);
 
-    var map = new google.maps.Map(document.getElementById("embedMap"), myOptions);
-    var marker = new google.maps.Marker({ position:latlong, map:map, title:"You are here!" });
-    setTimeout(showPosition(), 10000);
+    //Center the map on the user's location.
+    map.setView({ center: loc });
+}
+*/
 
+var tid;
+
+function startPush(){
+  tid = setTimeout(pushLoc, 2000);
 }
 
-// Define callback function for failed attempt
-function showError(error) {
-    if(error.code == 1) {
-        result.innerHTML = "You've decided not to share your position, but it's OK. We won't ask you again.";
-    } else if(error.code == 2) {
-        result.innerHTML = "The network is down or the positioning service can't be reached.";
-    } else if(error.code == 3) {
-        result.innerHTML = "The attempt timed out before it could get the location data.";
-    } else {
-        result.innerHTML = "Geolocation failed due to unknown error.";
-    }
+function pushLoc(){
+  var loc = new Microsoft.Maps.Location(lat, long);
+
+  //Update the user pushpin.
+  userPin.setLocation(loc);
+  userPin.setOptions({ visible: true });
+  console.log('Updated '+lat + ' ' +long);
+
+  //Center the map on the user's location.
+  map.setView({ center: loc });
+
+  tid = setTimeout(pushLoc, 5000);
+}
+
+function putPin(){
+  userPin = new Microsoft.Maps.Pushpin(map.getCenter(), { visible: false });
+    map.entities.push(userPin);
+}
+
+function StopTracking() {
+    getloc.unsubscribe();
+    clearTimeout(tid);
+    //Remove the user pushpin.
+    map.entities.clear();
 }
