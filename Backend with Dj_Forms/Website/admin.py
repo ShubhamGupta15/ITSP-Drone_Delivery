cb0a2b02949b69c7d6738da84e7b511fca8aa3a3
The Code Demon <ridayesh@gmail.com>
2020-06-23 02:32:25 +0530

    Added deploy pin

--- Backend with Dj_Forms/Website/admin.py
+++ Backend with Dj_Forms/Website/admin.py
@@ -6,6 +6,8 @@ from .models import Delivery_Order, Drone
 class OrderAdmin(admin.ModelAdmin):
     readonly_fields = ('date','time',)
 
+class DroneAdmin(admin.ModelAdmin):
+    readonly_fields = ('deploy_pin',)
 
 admin.site.register(Delivery_Order,OrderAdmin)
-admin.site.register(Drone)
+admin.site.register(Drone,DroneAdmin)
