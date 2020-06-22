e8051f2644c82f392261731120abfe489ec57b7c
The Code Demon <ridayesh@gmail.com>
2020-06-23 00:46:29 +0530

    CSS and drone timing done

--- Backend with Dj_Forms/static/JS/tracking.js
+++ Backend with Dj_Forms/static/JS/tracking.js
@@ -136,13 +136,24 @@ function deliveryDone(){
   if(reached==2 && alt < 1){
     document.getElementById("flash").innerHTML="<p>Your Delivery has reached its destination. Press Delivered button after collecting it.</p>"
 
-    document.getElementById("deliveredButton").innerHTML = "<input type = "submit" value="Delivered" name = "Delivered" class = "text" id = "Delivered-Button">";
+    document.getElementById("deliveredButton").innerHTML = "<input type = /"submit/" value=/"Delivered/" name = /"Delivered/" class = /"text/" id = /"Delivered-Button/">";
   }
   document.getElementById("longitude").innerText = long;
   document.getElementById("latitude").innerText = lat;
   document.getElementById("altitude").innerText = Math.round((alt)*10)/10 + 68.4;  
   trying = setTimeout(deliveryDone, 1000);
 }
+/*console.log("REACHED HERE")
+function asd(){
+
+  document.getElementById("flash").innerHTML="<p>Your Delivery has reached its destination. Press Delivered button after collecting it.</p>"
+
+  document.getElementById("deliveredButton").innerHTML = "<input type = \"submit\" value=\"Delivered\" name = \"Delivered\" class = \"text\" id = \"Delivered-Button\">"
+
+}*/
+
+setTimeout(asd,10000)
+console.log("Timer Set")
 
 function putPin(){
   userPin = new Microsoft.Maps.Pushpin(map.getCenter(), { visible: false });
