1d20cd1bd5ead0c16e0c3ab94718cf4d803e1dd9
Shubham <shubhi.g2001@gmail.com>
2020-06-25 23:30:56 +0530

    repository cleaned

--- AssignWP_0.h
+++
@@ -1,9 +0,0 @@
-#ifndef WAYPOINT_IN_0_H
-#define WAPOINT_IN_0_H
-#include <vector>
-#include <string>
-#include <mavros_msgs/Waypoint.h>
-
-std::vector<mavros_msgs::Waypoint> waypoint_in(std::string hostel);
-
-#endif
